8ab6b631fa7858d7eca0caa6637267833bcfb15b
meevivy01 <meevivy@gmail.com>
2026-01-17 08:57:09 +0700

    Update Git1.py

--- Git1.py
+++ Git1.py
@@ -95,8 +95,8 @@ if os.path.exists(CLIENTS_PATH):
 
 # --- TARGET CONFIG ---
 TARGET_UNIVERSITIES = ["หัวเฉียว"," Huachiew"]  
-TARGET_FACULTIES = ["เครื่องสำอาง","Cosmetic Science"] 
-TARGET_MAJORS = ["เครื่องสำอาง", "วิทยาศาสตร์เครื่องสำอาง","Cosmetic Science", "Cosmetics", "Cosmetic"]
+TARGET_FACULTIES = ["เครื่องสำอาง","Cosmetic"] 
+TARGET_MAJORS = ["เครื่องสำอาง", "Cosmetics", "Cosmetic"]
 SEARCH_KEYWORDS = ["หัวเฉียว เครื่องสำอาง","Cosmetic Huachiew"]
 
 # --- 🟢 เพิ่มชุดนี้ไว้ใต้ SEARCH_KEYWORDS ---
@@ -1692,5 +1692,6 @@ if __name__ == "__main__":
         exit()
     scraper = JobThaiRowScraper()
     scraper.run()
+
 
 
